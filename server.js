7591f04e828f2feaa7c30e57496ee24d5b5537e1
Charles <adeakinyemi129@gmail.com>
2025-03-22 17:19:29 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const http = require('http')
+const socketIo = require('socket.io')
+
+const app = express()
+
+const server = http.createServer(app)
+
+
+//initiate socket io and attach it to the http server
+const io = socketIo(server)
+
+//serve the static files from the public directory to express
+app.use(express.static('public'))
+
+const users = new Set()
+
+io.on('connection', (socket)=>{
+    console.log('A user is now connected')
+
+    //handle users when they will join the chat
+    socket.on('join', (username)=>{
+        users.add(username)
+
+        //emit a broadcast to all clients that a user joined
+        io.emit(`userJoined`, username)
+
+        //send the updated user list to all clients
+        io.emit('userList', Array.from(users))
+    })
+
+    //handle incoming messages
+    socket.on('chatMessage', (message)=>{
+        //broadcast received message to all clients connected
+        io.emit('chatMessage', message)
+    })
+
+    //handle when a user disconnects
+})
+
+server.listen(3000, ()=>{
+    console.log('Server is now listening on port 3000')
+})
